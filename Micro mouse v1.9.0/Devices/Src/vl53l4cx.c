eb5f6569f8cbc98f2296b958807c89a20f916ee0
76EHwan <112930692+76EHwan@users.noreply.github.com>
2026-01-17 02:28:49 +0900

    vl53l4cx 주소 변경 완료

--- Micro mouse v1.9.0/Devices/Src/vl53l4cx.c
+++ Micro mouse v1.9.0/Devices/Src/vl53l4cx.c
@@ -31,10 +31,18 @@ void VL53L4CX_Init() {
 	HAL_GPIO_WritePin(XSHUT0_GPIO_Port, XSHUT0_Pin, GPIO_PIN_SET);
 	HAL_Delay(5);
 
+	VL53LX_SetDeviceAddress(Dev, 0x56); // 예: 0x52 -> 0x56
+	Dev->I2cDevAddr = 0x56;
+
 	VL53LX_RdByte(Dev, 0x010F, &byteData);
 	LCD_Printf(0, 1, "Model_ID: %02X", byteData);
 	VL53LX_RdByte(Dev, 0x0110, &byteData);
 	LCD_Printf(0, 2, "Module_Type: %02X", byteData);
 	VL53LX_RdWord(Dev, 0x010F, &wordData);
 	LCD_Printf(0, 3, "VL53L4CX: %02X", wordData);
+
+	status = VL53LX_WaitDeviceBooted(Dev);
+	status = VL53LX_DataInit(Dev);
+	status = VL53LX_StartMeasurement(Dev);
+
 }
